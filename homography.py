278b12918fb41e1954a5af15dc0d9f1a35d25efa
davinderiitgn <42417335+davinderiitgn@users.noreply.github.com>
2018-10-19 08:33:03 +0530

    Update homography.py

--- homography.py
+++ homography.py
@@ -1,63 +1,111 @@
-import cv2
+'''The code to find matchings and keypoints is taken from "https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_feature_homography/py_feature_homography.html"'''
 import numpy as np
+import cv2
+import random
 
-images = {}
-for i in range(1,7):
-    images[i] = cv2.imread("img"+str(i)+".JPG",0)
+# DLT method to get homography matrix
+def matrix(P,Q):
+   A = np.zeros((8,9))
+   for i in range(4):
+       x,y = P[i]
+       u,v = Q[i]
+       A[2*i] = [x,y,1,0,0,0,-x*u,-y*u,-u]
+       A[2*i+1] = [0,0,0,x,y,1,-x*v,-y*v,-v]
 
-img_1 = cv2.imread('img1.JPG',cv2.IMREAD_GRAYSCALE)
-img_2 = cv2.imread('img2.JPG',cv2.IMREAD_GRAYSCALE)
-
-sift = cv2.xfeatures2d.SIFT_create()
-
-kp1,des1 = sift.detectAndCompute(img_1,None)
-kp2,des2 = sift.detectAndCompute(img_2,None)
-
-img_1 = cv2.drawKeypoints(img_1,kp1,None)
-img_2 = cv2.drawKeypoints(img_2,kp2,None)
-
-cv2.imwrite('sift_keypoints_1.jpg',img_1)
-cv2.imwrite('sift_keypoints_2.jpg',img_2)
-
-bf = cv2.BFMatcher_create(cv2.NORM_L2,crossCheck=False)
-matches = bf.match(des1,des2)
-
-matches = sorted(matches, key = lambda x:x.distance)
-matching_resulst = cv2.drawMatches(img_1,kp1,img_2,kp2,matches[:100],None)
-
-cv2.imwrite("result.jpg",matching_resulst)
-
-# getting keypoints
-kyp_1 = []
-kyp_2 = []
-for i in range(100):
-    ind1,ind2 = matches[i].trainIdx, matches[i].queryIdx
-    kyp_1.append(kp1[ind2].pt)
-    kyp_2.append(kp2[ind1].pt)
+   U, S, V = np.linalg.svd(A, full_matrices=False)
+   A_ = V[-1]
+   H = np.reshape(A_, (3, 3))
+   H = H/H.item(8)
+   return H
 
 
-# Getting Homography Matrix
+def RANSAC(src_pts, dst_pts, n, tolerance): #n = number of iterations
+    itr_inliers = {} #store inliers for each iteration
+    itr_homos = {} #store homography matrix for each iteration and then later choose the best one
+    for j in range(n):
+        indices = random.sample(range(len(src_pts)), 4) #pick four point at random
+        P = []
+        Q = []
+        for index in indices:
+            P.append((src_pts[index][0][0],src_pts[index][0][1]))
+            Q.append((dst_pts[index][0][0], dst_pts[index][0][1]))
+        estimate = matrix(P,Q) # generate homography matrix using DLT
+        inliers = [] #contains src_pts , dst_pts pairs which are inliers
+        #calculating inliers
+        for i in range(len(src_pts)):
+            X = np.array([[int(src_pts[i][0][0])],[int(src_pts[i][0][1])],[1]])
+            V_estimate = np.dot(estimate,X)
+            V_estimate = (1/V_estimate[2])*V_estimate
 
-#h, status = cv2.findHomography(pts_src, pts_dst) #inbuilt function for reference
+            V = np.array([[dst_pts[i][0][0]],[dst_pts[i][0][1]],[1]])
 
+            if np.linalg.norm(V_estimate-V)<=tolerance:
+                inliers.append([src_pts[i],dst_pts[i]])
+        itr_inliers[j] = inliers
+        itr_homos[j] = estimate
+    T = 0.95 #threshold for percentage of inliers
+    total = len(src_pts)
+    max = -1
+    final_id = 0
+    for id in itr_inliers.keys():
+        num_inl = len(itr_inliers[id])
+        perc_inliers = num_inl/float(total)
+        if perc_inliers>=T:
+            final_id = id
+            break #break out of the for loop
+        else:
+            if perc_inliers>max:
+                final_id = id
+                max = perc_inliers
+    final_inliers = itr_inliers[final_id]
+    if len(final_inliers)<4: #If number of max inliers is less than 4 than return homography matrix direclty.
+                            # It should be noted that the estimate in this case would not be reliable
+        return None, None
+    elif len(final_inliers)%2==0:
+        rn = len(final_inliers)
+    else:
+        rn = len(final_inliers)-1
 
-# Using DLT Algorithm
-def homography(P,Q):
-    A = np.zeros((8,9))
-    for i in range(4):
-        x,y = P[i]
-        u,v = Q[i]
-        #A[2*i] = [-x,-y,-1,0,0,0,u*x,u*y,u]
-        #A[2*i+1] = [0,0,0,-x,-y,-1,x*v,y*x,v]
-        A[2*i] = [x,y,1,0,0,0,-u*x,-u*y,-u]
-        A[2*i+1] = [0,0,0,x,y,1,x*v,y*x,v]
-
-    [U, S, V] = np.linalg.svd(A)
-    A_ = V[-1, :]
+    A_final = np.zeros((2*rn, 9))
+    for k in range(rn):
+        x,y = final_inliers[k][0][0][0],final_inliers[k][0][0][1]
+        u,v = final_inliers[k][1][0][0],final_inliers[k][1][0][1]
+        A_final[2 * k] = [x, y, 1, 0, 0, 0, -x * u, -y * u, -u]
+        A_final[2 * k + 1] = [0, 0, 0, x, y, 1, -x * v, -y * v, -v]
+    U, S, V = np.linalg.svd(A_final, full_matrices=False)
+    A_ = V[-1]
     H = np.reshape(A_, (3, 3))
-    return H
+    H = H/H.item(8)  # Normalize
+    return H,len(final_inliers)
 
-P = [(int(pt[0]),int(pt[1])) for pt in kyp_1[:4]]
-Q = [(int(pt[0]),int(pt[1])) for pt in kyp_2[:4]]
-print P,Q
-print homography(P,Q)
+def homography(img1,img2):
+   sift = cv2.xfeatures2d.SIFT_create()
+   kp1, des1 = sift.detectAndCompute(img1,None)
+   kp2, des2 = sift.detectAndCompute(img2,None)
+
+   FLANN_INDEX_KDTREE = 0
+   index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+   search_params = dict(checks = 50)
+   flann = cv2.FlannBasedMatcher(index_params, search_params)
+   matches = flann.knnMatch(des1,des2,k=2)
+   # store all the good matches as per Lowe's ratio test.
+   good = []
+   for m,n in matches:
+       if m.distance < 0.7*n.distance:
+           good.append(m)
+   MIN_MATCH_COUNT = 10
+   if len(good)>MIN_MATCH_COUNT:
+       src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+       dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+       flag = None #the RANSAC function returns NONE in case the key points are very low(<4), in such case we run the Function once again to get better estimate
+       while(flag==None):
+           try:
+               M, I = RANSAC(src_pts, dst_pts, 5000, 5)
+               flag=I
+           except:
+               #try again key points are very low in case key points are very low
+               #helps to get a good estimate of homography matrix
+               flag=1
+   else:
+       print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+return M
